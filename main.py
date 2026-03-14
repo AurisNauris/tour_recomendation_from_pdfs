78d51d73320b8c4f59166c02f5865c8c0da753a3
Aurimas <aurimasnarkevicius@gmail.com>
2026-03-14 21:20:27 +0100

    Adding chunked data to a database saved locally

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pymupdf
 from pathlib import Path
 from dotenv import load_dotenv
-
+import chromadb
 #load_dotenv()
 
 def extract_text_from_pdf(pdf_path):
@@ -24,10 +24,39 @@ def simple_chunking(text):
     
     return chunks
 
+def add_chunks_to_collection(collection, file_name, chunks):
+    
+    print(f"Putting text extracted from s{file_name} into database")
+    existing = collection.get(where={"source":file_name})
+    if len(existing["ids"]) > 0:
+        print(f"Skipping {file_name}, already in database")
+        return
+    
+    ids = [f"{file_name}_{i}" for i in range(len(chunks))]
+    collection.add(ids=ids,
+                documents=chunks,
+                metadatas=[{"source":file_name} for _ in chunks])
+    
+def peek_at_collection(collection):
+    print("Total items: ", collection.count())
+    # peek at a few entries
+    sample = collection.peek()
+    for i in range(min(3, len(sample["ids"]))):
+        print(f"ID: {sample['ids'][i]}")
+        print(f"Source: {sample['metadatas'][i]['source']}")
+        print(f"Text: {sample['documents'][i][:100]}...")
+
 
 guide_dir = Path("guides")
 pdf_file_paths = list(guide_dir.glob("*.pdf"))
-
-pdf_extracted = [extract_text_from_pdf(pdf_path) for pdf_path in pdf_file_paths[2:]]
+file_names = [fp.stem for fp in pdf_file_paths]
+pdf_extracted = [extract_text_from_pdf(pdf_path) for pdf_path in pdf_file_paths]
 chunked_text = [simple_chunking(source) for source in pdf_extracted]
-print(len(chunked_text))
+
+chroma_client = chromadb.PersistentClient(path="./chroma_db")
+collection = chroma_client.get_or_create_collection(name="travel_guides")
+
+for file_name, chunks in zip(file_names, chunked_text):
+    add_chunks_to_collection(collection, file_name, chunks)
+
+peek_at_collection(collection)
